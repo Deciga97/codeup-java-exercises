4860f8173b33b1dbe6356cfcad902a2625510715
Steve <stevedee97@gmail.com>
2022-12-08 15:01:31 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Java Class //
+public class Main {
+//    Java Method //
+    public static void main(String[] args) {
+        // console.log //
+        System.out.println("Hello World!");
+        // Main Method Shortcut //
+        // Main Method Shortcut //
+        // Main(tah) //
+        // psvm (tab/enter) //
+    }
+}
+
+// System.out.println shortcut //
+// sout //
