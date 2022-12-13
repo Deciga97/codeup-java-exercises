3ce0e80d739140415d73b2878ed8611c8b5e78e7
Steve <stevedee97@gmail.com>
2022-12-13 12:47:02 -0600

    fizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,35 +1,47 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while(i <= 15) {
-            System.out.println(i);
-            i++;
-        }
+        // While Loop //
+//        int i = 5;
+//        while(i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
 
+        // Do While Loop //
         int x = 100;
         do{
             x -= 5;
             System.out.println(x);
         } while(x >= -10);
 
-        for (int x = 100; x >= -10; x -= 5) {
-            System.out.println(x);
-        }
+        //Refactor do while loop to for loop //
 
+//        for (int x = 100; x >= -10; x -= 5) {
+//            System.out.println(x);
+//        }
 
+        // Do While Loop //
         int number = 2;
         do {
-            // Display the number squared on each line
             System.out.println(number * number);
-            // Increment the number by 1
             number++;
         } while (number < 66000);
 
-        //Refactor to For Loop //
-        for (int number = 2; number < 1000000; number++) {
-            // Display the number squared on each line
-            System.out.println(number * number);
+        //Refactor do while loop to for loop //
+//        for (int number = 2; number < 1000000; number++) {
+//            // Display the number squared on each line
+//            System.out.println(number * number);
+//        }
+
+        // FizzBuzz //
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
         }
+
 
 
 
