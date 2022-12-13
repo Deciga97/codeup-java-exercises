e01de83f92060814f821c057346ea00f2a8a5732
Steve <stevedee97@gmail.com>
2022-12-13 13:41:16 -0600

    complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
         // While Loop //
@@ -42,8 +44,65 @@ public class ControlFlowExercises {
             }
         }
 
+        // table of powers //
+                Scanner scanner = new Scanner(System.in);
+
+                // Prompt the user to enter an integer
+                System.out.print("What number would you like to go up to? ");
+                int n = scanner.nextInt();
+
+                // Display the table header
+                System.out.println("Here is your table!");
+                System.out.println("number | squared | cubed");
+                System.out.println("------ | ------- | -----");
+
+                // Display the table rows
+                for (int i = 1; i <= n; i++) {
+                    System.out.printf("%d      | %d       | %d\n", i, i * i, i * i * i);
+                }
+
+                // Ask if the user wants to continue
+                System.out.print("Would you like to continue? (y/n) ");
+                String answer = scanner.next();
+
+                // Only continue if the user agrees to
+                if (answer.equalsIgnoreCase("y")) {
+                    // TODO: Add code here to continue3 the program
+                }
+
+        // Prompt the user for a numerical grade
+        System.out.print("Enter a numerical grade from 0 to 100: ");
+        int grade = scanner.nextInt();
+
+        // Display the corresponding letter grade
+        if (grade >= 88) {
+            System.out.println("The letter grade is: A");
+        } else if (grade >= 80) {
+            System.out.println("The letter grade is: B");
+        } else if (grade >= 67) {
+            System.out.println("The letter grade is: C");
+        } else if (grade >= 60) {
+            System.out.println("The letter grade is: D");
+        } else {
+            System.out.println("The letter grade is: F");
+        }
+
+        // Ask if the user wants to continue
+        System.out.print("Would you like to continue? (y/n) ");
+        String userAnswer = scanner.next();
+
+        // Only continue if the user agrees to
+        if (userAnswer.equalsIgnoreCase("y")) {
+            System.out.println("Jk");
+        }
+
+            }
 
 
 
-    }
+
+
+
+
 }
+
