52ffd3f9bdebe79a9932d2298d802543dfd33344
Steve <stevedee97@gmail.com>
2023-01-02 12:17:30 -0600

    stopping point

---
+++ src/util/Input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return this.scanner.nextLine();
+    }
+}
