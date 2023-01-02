d22b316e89f9e2d42db5b367b68a9cb8faea9001
Steve <stevedee97@gmail.com>
2023-01-02 11:48:32 -0600

    first commit

---
+++ src/Person.java
@@ -0,0 +1,31 @@
+
+public class Person {
+        private String name;
+
+        // Constructor
+        public Person(String name) {
+                this.name = name;
+        }
+
+        public String getName(){
+                this.name = name;
+        }
+
+        public void setName(String name){
+                this.name = name;
+        }
+        public void sayHello(){
+                System.out.println("Hello, my name is " + this.name);
+        }
+
+        public static void main(String[] args) {
+                Person person = new Person("Alice");
+                System.out.println(person.getName());  // Output: "Alice"
+                person.sayHello();  // Output: "Hello, my name is Alice"
+                person.setName("Bob");
+                System.out.println(person.getName());  // Output: "Bob"
+        }
+}
+
+
+
