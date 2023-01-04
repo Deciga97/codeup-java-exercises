2e3230a21caa7242c6ad1d3dce926670ef0de5d0
Steve <stevedee97@gmail.com>
2023-01-04 14:02:32 -0600

    contro flow ex

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -17,13 +17,13 @@ public class ConsoleExercises {
         System.out.println("You entered: " + number); // print the value of the number variable
 
         // Prompting User to pick 3 words //
-        System.out.print("Pick the first number");
+        System.out.print("Pick the first word: ");
         String firstWord = scanner.nextLine();
 
-        System.out.print("Pick the second number");
+        System.out.print("Pick the second word: ");
         String secondWord = scanner.nextLine();
 
-        System.out.print("Pick the third number");
+        System.out.print("Pick the third word: ");
         String thirdWord = scanner.nextLine();
 
         System.out.println(firstWord);
@@ -31,46 +31,33 @@ public class ConsoleExercises {
         System.out.println(thirdWord);
 
         // Sentence //
-
         System.out.print("Please enter a sentence: ");
-        String sentence = scanner.next();
-        System.out.println("You entered " + sentence);
-
-        System.out.print("Please enter a sentence: "); // prompt the user to enter a sentence
-        String userInput = scanner.next(); // read the sentence using the next method
-
-        System.out.println("You entered: " + userInput); // print the value of the sentence variable
+        String sentence = scanner.nextLine();
+        System.out.println("You entered: " + sentence);
 
         // NextLine //
         System.out.print("Please enter a sentence: "); // prompt the user to enter a sentence
-        String sentence = scanner.nextLine(); // read the sentence using the nextLine method
+        String userInput = scanner.nextLine(); // read the sentence using the nextLine method
 
-        System.out.println("You entered: " + sentence); // print the value of the sentence variable
+        System.out.println("You entered: " + userInput); // print the value of the sentence variable
 
         // Measurement //
-                // Prompt the user to enter the length
-                System.out.println("Enter the length of the classroom: ");
-                String lengthInput = scanner.nextLine();
-                double length = Double.parseDouble(lengthInput);
+        // Prompt the user to enter the length
+        System.out.println("Enter the length of the classroom: ");
+        String lengthInput = scanner.nextLine();
+        double length = Double.parseDouble(lengthInput);
 
-                // Prompt the user to enter the width
-                System.out.println("Enter the width of the classroom: ");
-                String widthInput = scanner.nextLine();
-                double width = Double.parseDouble(widthInput);
+        // Prompt the user to enter the width
+        System.out.println("Enter the width of the classroom: ");
+        String widthInput = scanner.nextLine();
+        double width = Double.parseDouble(widthInput);
 
-                // Calculate the area and perimeter
-                double area = length * width;
-                double perimeter = 2 * length + 2 * width;
+        // Calculate the area and perimeter
+        double area = length * width;
+        double perimeter = 2 * length + 2 * width;
 
-                // Print the results
-                System.out.println("The area of the classroom is " + area);
-                System.out.println("The perimeter of the classroom is " + perimeter);
-            }
-        }
-
+        // Print the results
+        System.out.println("The area of the classroom is " + area);
+        System.out.println("The perimeter of the classroom is " + perimeter);
     }
-
-
-
-}
 }
--- src/bob.java
+++ src/bob.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Bob {
+class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input;
