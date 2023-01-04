05a2769b3d66fdd9998fabcde230eb23346c9f3e
Steve <stevedee97@gmail.com>
2023-01-04 13:41:22 -0600

    java syntax

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,20 @@
+// This is a Java Class //
 public class HelloWorld {
+
+    // This is a Main Method; In this Method is blocks of code to run on terminal //
     public static void main (String[] args) {
+
+        // Creating an Integer Variable (Int) called myFavoriteNumber //
+        int myFavoriteNumber = 18;
+        // Printing out my favorite number to console //
+        System.out.println("My favorite number is " + myFavoriteNumber + ".");
+        // Creating a String Variable named myString //
+        String myString = "I sleep before I wake.";
+        // Printing out my string to console //
+        System.out.println(myString);
+        //Declaring long variable named myNumber //
+        Long myNumber;
+
         System.out.println("Hello World");
     }
 }
