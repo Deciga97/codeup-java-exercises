53fe9c84f7966fff5ead1295b9af082aa35c94cc
Steve <stevedee97@gmail.com>
2023-01-04 13:48:45 -0600

    complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -13,7 +13,35 @@ public class HelloWorld {
         // Printing out my string to console //
         System.out.println(myString);
         //Declaring long variable named myNumber //
-        Long myNumber;
+        double myNumber = 3.14;
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+        int three = (int) "three";
+
+        int x = 4;
+        x += 5;
+
+        int x = 3;
+        int y = 4;
+        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+
 
         System.out.println("Hello World");
     }
