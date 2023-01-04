7966b8b9b22fc7add3ed95fa5edaca950b937660
Steve <stevedee97@gmail.com>
2023-01-04 14:56:24 -0600

    complete

---
+++ src/shapes/Circle.java
@@ -0,0 +1,36 @@
+package shapes;
+import java.util.Scanner;
+
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public class CircleApp {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter the radius of the circle: ");
+            double radius = input.nextDouble();
+
+            Circle circle = new Circle(radius);
+            double circumference = circle.getCircumference();
+            double area = circle.getArea();
+
+            System.out.println("The circumference of the circle is: " + circumference);
+            System.out.println("The area of the circle is: " + area);
+        }
+    }
+
+
+}
